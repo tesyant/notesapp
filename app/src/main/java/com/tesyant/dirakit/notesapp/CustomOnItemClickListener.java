1cf56f68407df56bf4f5439fae7523fdd230d9a5
tesya <tesyant@gmail.com>
2017-09-12 11:39:50 +0700

    first commit

---
+++ app/src/main/java/com/tesyant/dirakit/notesapp/CustomOnItemClickListener.java
@@ -0,0 +1,24 @@
+package com.tesyant.dirakit.notesapp;
+
+import android.view.View;
+
+/**
+ * Created by tesyant on 9/12/17.
+ */
+
+public class CustomOnItemClickListener implements View.OnClickListener {
+    private int position;
+    private OnItemClickCallback onItemClickCallback;
+    public CustomOnItemClickListener(int position, OnItemClickCallback onItemClickCallback) {
+        this.position = position;
+        this.onItemClickCallback = onItemClickCallback;
+    }
+
+    @Override
+    public void onClick(View view) {
+        onItemClickCallback.onItemClicked(view, position);
+    }
+    public interface OnItemClickCallback {
+        void onItemClicked(View view, int position);
+    }
+}
